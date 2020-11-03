bf20f8db51f8968dde79a37e894d5df0caf5e550
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2020-11-03 21:13:34 +0530

    Navigator 2 & 3

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import { View, Platform } from 'react-native';
-import { DISHES } from '../shared/dishes';
 import { createStackNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({
@@ -22,13 +22,46 @@ const MenuNavigator = createStackNavigator({
   }
 }
 );
+
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
+    },
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
+const MainNavigator = createDrawerNavigator({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, {
+  drawerBackgroundColor: '#D1C4E9'
+});
 class Main extends Component {
   
   render() {
  
     return (
       <View style={{flex:1, paddingTop: Platform.OS === 'android' ? 0 : Expo.Constants.statusBarHeight }}>
-        <MenuNavigator />
+        <MainNavigator />
       </View>
     );
   }
